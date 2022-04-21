63488dd501f9374ab3d37fa7455c45a3ae33e626
Jakub <dsw45521@student.edu.pl>
2022-04-21 23:08:15 +0200

    jakis opis

---
+++ Desktop/Lab3.sh
@@ -0,0 +1,39 @@
+git commit
+git commit
+git branch bugFix
+git checkout bugFix
+git checkout -b bugFix
+git commit
+git checkout master
+git commit
+git merge bugFix
+git checkout -b bugFix
+git commit
+git checkout master
+git commit
+git checkout bugFix
+git rebase master
+git checkout C4
+git checkout C4^
+git branch -f master C6
+git branch -f bugFix C0
+git checkout C1
+git reset local~1
+git checkout pushed
+git revert pushed
+git cherry-pick C3 C4 C7
+git rebase -i master~4 --aboveAll
+git checkout master
+git cherry-pick C4
+git rebase -i caption~2 --aboveAll
+git commit --amend
+git rebase -i caption~2 --aboveAll
+git branch -f master caption
+git checkout master
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+git tag v0 C1
+git tag v1 C2
+git checkout C2
+git commit
